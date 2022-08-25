b74acd56186631be200fd53bec2c55ebea0ea1c7
Eugenio <106436564+eugeniobalbi@users.noreply.github.com>
2022-08-25 10:57:45 -0300

    Add files via upload

---
+++ TimeCalculator/main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 25 10:48:34 2022
+
+@author: eugen
+"""
+
+from time_calculator import add_time
+from unittest import main
+
+
+print(add_time("11:06 PM", "2:02"))
+
+
+#Correr TEST
+main(module='test_module', exit=False)
