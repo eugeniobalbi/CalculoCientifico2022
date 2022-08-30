6bcb711821f20533a5cbc3f552675373750d33c1
Eugenio <106436564+eugeniobalbi@users.noreply.github.com>
2022-08-30 18:30:19 -0300

    Add files via upload

---
+++ Probability Calculator/main.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 30 18:25:01 2022
+
+@author: eugen
+"""
+
+import prob_calculator
+from unittest import main
+
+hat = prob_calculator.Hat(blue=4, red=2, green=6)
+probability = prob_calculator.experiment(
+    hat=hat,
+    expected_balls={"blue": 2,
+                    "red": 1},
+    num_balls_drawn=4,
+    num_experiments=3000)
+print("Probability:", probability)
+
+main(module='test_module', exit=False)
+
+
