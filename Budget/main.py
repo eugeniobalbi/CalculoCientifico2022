cf871a6017359ca1488b9d466d536db98c605939
Eugenio <106436564+eugeniobalbi@users.noreply.github.com>
2022-08-30 18:16:57 -0300

    Add files via upload

---
+++ Budget/main.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 30 17:54:27 2022
+
+@author: eugen
+"""
+
+import budget
+from budget import create_spend_chart
+from unittest import main
+
+food = budget.Category("Food")
+food.deposit(1000, "initial deposit")
+food.withdraw(10.15, "groceries")
+food.withdraw(15.89, "restaurant and more food for dessert")
+print(food.get_balance())
+clothing = budget.Category("Clothing")
+food.transfer(50, clothing)
+clothing.withdraw(25.55)
+clothing.withdraw(100)
+auto = budget.Category("Auto")
+auto.deposit(1000, "initial deposit")
+auto.withdraw(15)
+
+print(food)
+print(clothing)
+
+print(create_spend_chart([food, clothing, auto]))
+
+# Correr Test!
+main(module='test_module', exit=False)
+
+
